369cc543c817e05775e13ce6cbb8e5a7faf60761
LadyEebs <ladyeebs@gmail.com>
2025-01-25 09:42:56 -0500

    Use textureLod for bloom pass

--- resource/shaders/bloom_f.glsl
+++ resource/shaders/bloom_f.glsl
@@ -13,15 +13,15 @@ void main(void)
 	vec2 PixSize = param1 / iResolution.xy;
     
 	// 6x6 downscale/upscale
-    vec4 s0 = texture(tex, f_uv.xy + vec2( 0.000000000, 0.000000000) * PixSize.xy, 0.0);
-	vec4 s1 = texture(tex, f_uv.xy + vec2( 0.604077935, 0.000000000) * PixSize.xy, 0.0);
-	vec4 s2 = texture(tex, f_uv.xy + vec2( 0.000000000, 0.604077935) * PixSize.xy, 0.0);
-	vec4 s3 = texture(tex, f_uv.xy + vec2(-0.604077935, 0.000000000) * PixSize.xy, 0.0);
-	vec4 s4 = texture(tex, f_uv.xy + vec2( 0.000000000,-0.604077935) * PixSize.xy, 0.0);		
-	vec4 s5 = texture(tex, f_uv.xy + vec2( 0.604077935, 0.604077935) * PixSize.xy, 0.0);
-	vec4 s6 = texture(tex, f_uv.xy + vec2(-0.604077935, 0.604077935) * PixSize.xy, 0.0);
-	vec4 s7 = texture(tex, f_uv.xy + vec2( 0.604077935,-0.604077935) * PixSize.xy, 0.0);
-	vec4 s8 = texture(tex, f_uv.xy + vec2(-0.604077935,-0.604077935) * PixSize.xy, 0.0);
+    vec4 s0 = textureLod(tex, f_uv.xy + vec2( 0.000000000, 0.000000000) * PixSize.xy, 0.0);
+	vec4 s1 = textureLod(tex, f_uv.xy + vec2( 0.604077935, 0.000000000) * PixSize.xy, 0.0);
+	vec4 s2 = textureLod(tex, f_uv.xy + vec2( 0.000000000, 0.604077935) * PixSize.xy, 0.0);
+	vec4 s3 = textureLod(tex, f_uv.xy + vec2(-0.604077935, 0.000000000) * PixSize.xy, 0.0);
+	vec4 s4 = textureLod(tex, f_uv.xy + vec2( 0.000000000,-0.604077935) * PixSize.xy, 0.0);		
+	vec4 s5 = textureLod(tex, f_uv.xy + vec2( 0.604077935, 0.604077935) * PixSize.xy, 0.0);
+	vec4 s6 = textureLod(tex, f_uv.xy + vec2(-0.604077935, 0.604077935) * PixSize.xy, 0.0);
+	vec4 s7 = textureLod(tex, f_uv.xy + vec2( 0.604077935,-0.604077935) * PixSize.xy, 0.0);
+	vec4 s8 = textureLod(tex, f_uv.xy + vec2(-0.604077935,-0.604077935) * PixSize.xy, 0.0);
 		
 	vec4 Color = s0 * 0.145904019;
 	Color += 0.11803490998 * (s1 + s2 + s3 + s4);
